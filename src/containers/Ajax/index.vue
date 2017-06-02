d7c1ba1782491fcae6daf2b0ffcaed78d6a396fa
Pickcle <348707253@qq.com>
2017-06-02 18:44:18 +0800

    import axios module

---
+++ src/containers/Ajax/index.vue
@@ -0,0 +1,30 @@
+<template lang="jade">
+  div.ajax
+    button(@click="onGetClick") get
+    button(@click="onPostClick") post
+    p {{data_get}}
+    p {{data_post}}
+</template>
+
+<script>
+  import axios from 'axios'
+
+  export default {
+    data () {
+      return {
+        data_get: 'Nothing',
+        data_post: 'Nothing'
+      }
+    },
+    methods: {
+      onGetClick () {
+      },
+
+      onPostClick() {
+      }
+    }
+  }
+</script>
+
+<style lang="stylus">
+</style>
