bb4d0f902ad24ed48e148733cc57f907ca506988
Pickcle <348707253@qq.com>
2017-06-07 22:37:46 +0800

    CounterButton

--- src/containers/Countdown/index.vue
+++ src/containers/Countdown/index.vue
@@ -3,6 +3,7 @@
     h2 {{count}}
     button(@click="onResetClick") reset
     button(@click="onStateClick") state
+    button.counter-button(@click="onCounterBtnClick", :class="{ cooldown: isCooldowning }", v-text="cooldownText") button
 </template>
 
 <script>
@@ -11,10 +12,25 @@
 
   export default {
     computed: {
+      cooldownText () {
+        if (this.isCooldowning) {
+          return this.second + '秒'
+        } else {
+          return '发送'
+        }
+      },
+
       ...mapState({
         count: state => state.countdown.count,
         state: state => state
       })
+    },
+
+    data () {
+      return {
+        second: 5,
+        isCooldowning: false
+      }
     },
 
     methods: {
@@ -37,6 +53,36 @@
         if (this.timer) {
           clearInterval(this.timer)
           this.timer = null
+        }
+      },
+
+      onCounterBtnClick () {
+        if (this.isCooldowning) {
+          return
+        }
+        this.startCooldown()
+      },
+
+      startCooldown () {
+        this.stopCooldown()
+        this.isCooldowning = true
+        this.cooldownTimer = setInterval(() => {
+          this.cooldownTick()
+        }, 1000)
+      },
+
+      stopCooldown () {
+        if (this.cooldownTimer) {
+          clearInterval(this.cooldownTimer)
+          this.second = 5
+          this.isCooldowning = false
+        }
+      },
+
+      cooldownTick () {
+        this.second--
+        if (this.second <= 0) {
+          this.stopCooldown()
         }
       },
 
@@ -66,4 +112,16 @@
 </script>
 
 <style lang="stylus">
+  .counter-button
+    width: 104px
+    height: 32px
+    margin: 16px 0
+    background-color: #23ade5
+    border: none
+    border-radius: 4px
+    color: #fff
+
+    &.cooldown
+      background-color: #e9eaec
+      color: #b4b4b4
 </style>
